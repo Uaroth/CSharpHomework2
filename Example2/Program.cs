c4c1084b8c318e25c0ddf128455a6a68eb74c01d
Sergey <melnikov_s_a@mail.ru>
2022-11-04 16:26:40 +0300

    Example2

---
+++ Example2/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет
+Console.WriteLine("Здравствуйте! Введите целое число");
+int wholeNumber = Convert.ToInt32(Console.ReadLine());
+if (wholeNumber / 100 >= 1)
+{
+    int thirdNumber = wholeNumber % 10;
+    Console.WriteLine($"Третья цифра числа = {thirdNumber} ");
+}
+else Console.WriteLine("У числа нет третьей цифры");
